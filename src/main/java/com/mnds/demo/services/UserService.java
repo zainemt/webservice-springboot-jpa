919d1b73ea5b140e300cb568479763a6e9a9ce02
Zaine Mendes <zainemendes@hotmail.com>
2024-09-21 13:04:20 -0300

    Update user

--- src/main/java/com/mnds/demo/services/UserService.java
+++ src/main/java/com/mnds/demo/services/UserService.java
@@ -31,4 +31,16 @@ public class UserService {
 	public void delete(Long id) {
 		repository.deleteById(id);
 	}
+	
+	public User update(Long id, User user) {
+		User entity = repository.getReferenceById(id); //instancia um objeto monitorado pelo jpa, sem ir no banco de dados
+		updateData(entity, user);
+		return repository.save(entity);
+	}
+
+	private void updateData(User entity, User user) {
+		entity.setName(user.getName());
+		entity.setEmail(user.getEmail());
+		entity.setPhone(user.getPhone());
+	}
 }
